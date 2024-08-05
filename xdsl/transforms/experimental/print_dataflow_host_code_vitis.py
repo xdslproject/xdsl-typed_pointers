6e847237b8c06e0d9d3c1ea4f8c95a1aec170f46
Gabriel Rodriguez-Canal <gabriel.rodriguez.canal@pnnl.gov>
2024-08-04 20:12:03 -0700

    Fix errors in host code generator

--- xdsl/transforms/experimental/print_dataflow_host_code_vitis.py
+++ xdsl/transforms/experimental/print_dataflow_host_code_vitis.py
@@ -4,7 +4,6 @@ from xdsl.dialects import builtin
 from xdsl.dialects.experimental import dataflow
 #from xdsl.transforms.experimental.dataflow_to_func import ConvertDataflowToFunc
 
-from xdsl import traits
 from xdsl.ir import (
     MLContext,
 )
@@ -32,7 +31,7 @@ class GenerateGraphFromConnected(RewritePattern):
         out_nodes_lst = connected_op.out_nodes.data
 
         if in_nodes_lst:
-            events_in_waitlist = ",".join([f"&{in_node_symbol.root_reference.data}_done" for in_node_symbol in in_nodes_lst])
+            events_in_waitlist = ",".join([f"{in_node_symbol.root_reference.data}_done" for in_node_symbol in in_nodes_lst])
             self.events_waitlist[0] += f"cl_event {node.root_reference.data}_waitlist[] = {{{events_in_waitlist}}};\n\t"
 
             node_name = node.root_reference.data
@@ -60,26 +59,26 @@ class GenerateKernelsAndBuffersCode(RewritePattern):
         self.host_arrays[0] += f"float * out_{node_name} = (float*)malloc(N * sizeof(float));\n\t"
         self.init_host_arrays[0] += f"for(int i = 0; i < N; i++) out_{node_name}[i] = 0;\n\t"
         self.iter_vars[0] += f"cl_long iters_{node_name} = N;\n\t"
-        self.create_kernel[0] += f"cl_kernel node_0_kernel = clCreateKernel(program, \"{node_name}\", &err);\n\t"
+        self.create_kernel[0] += f"cl_kernel {node_name}_kernel = clCreateKernel(program, \"{node_name}\", &err);\n\t"
         self.create_buffer[0] += f"cl_mem out_{node_name}_buf = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, N * sizeof(cl_float), out_{node_name}, NULL);\n\t"
         if node_name == "node_0":
             self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_0_kernel, 0, sizeof(cl_mem), &in_buf);\n\t"
             self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_0_kernel, 1, sizeof(cl_mem), &out_node_0_buf);\n\t"
-            self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_0_kernel, 2, sizeof(cl_long), iters_{node_name});\n\t"
+            self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_0_kernel, 2, sizeof(cl_long), &iters_{node_name});\n\t"
         elif node_name == "node_4": # TODO: this works for the example with 5 nodes, but we should count the number of nodes to make this generic
             for n_pred,pred_node in enumerate(self.depends[node_name]):
                 self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_4_kernel, {n_pred}, sizeof(cl_mem), &out_{pred_node}_buf);\n\t"
             self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_4_kernel, {n_pred+1}, sizeof(cl_mem), &out_buf);\n\t"
-            self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_4_kernel, {n_pred+2}, sizeof(cl_long), iters_{node_name});\n\t"
+            self.set_kernel_arg[0] += f"err  = clSetKernelArg(node_4_kernel, {n_pred+2}, sizeof(cl_long), &iters_{node_name});\n\t"
         else:
             for n_pred,pred_node in enumerate(self.depends[node_name]):
                 self.set_kernel_arg[0] += f"err  = clSetKernelArg({node_name}_kernel, {n_pred}, sizeof(cl_mem), &out_{pred_node}_buf);\n\t"
-            self.set_kernel_arg[0] += f"err  = clSetKernelArg({node_name}_kernel, {n_pred+1}, sizeof(cl_long), iters_{node_name});\n\t"
+            self.set_kernel_arg[0] += f"err  = clSetKernelArg({node_name}_kernel, {n_pred+1}, sizeof(cl_long), &iters_{node_name});\n\t"
 
         if node_name in self.depends:
-            self.enqueue_kernel[0] += f"err = clEnqueueTask(commands, {node_name}_kernel, {len(self.depends[node_name])}, {node_name}_waitlist, &{node_name}_finish);\n\t"
+            self.enqueue_kernel[0] += f"err = clEnqueueTask(commands, {node_name}_kernel, {len(self.depends[node_name])}, {node_name}_waitlist, &{node_name}_done);\n\t"
         else:
-            self.enqueue_kernel[0] += f"err = clEnqueueTask(commands, {node_name}_kernel, 0, NULL, &{node_name}_finish);\n\t"
+            self.enqueue_kernel[0] += f"err = clEnqueueTask(commands, {node_name}_kernel, 0, NULL, &{node_name}_done);\n\t"
 
 
 def print_boilerplate(host_arrays, init_host_arrays, iter_vars, create_kernel, create_buffer, set_kernel_arg, events, events_waitlist, enqueue_kernel):
@@ -155,6 +154,8 @@ def print_boilerplate(host_arrays, init_host_arrays, iter_vars, create_kernel, c
                       \n
         {create_kernel}
                       \n
+        cl_mem in_buf = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, N * sizeof(cl_float), in, NULL);\n\t
+        cl_mem out_buf = clCreateBuffer(context,  CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, N * sizeof(cl_float), out, NULL);\n\t
         {create_buffer}
                       \n
         {set_kernel_arg}
